1c2b28473d0d72b0bd0534595a2b8b3a41e0220c
Harsh Kumar <hadron43@yahoo.com>
2022-12-09 12:26:43 +0530

    complete eval-fen

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,8 @@ DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = [ '*' ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 # Application definition
 
@@ -51,16 +53,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
--- core/views.py
+++ core/views.py
@@ -2,14 +2,23 @@ from django.http import JsonResponse
 from django.conf import settings
 import chess
 import chess.engine
+import json
 
 
 def eval_fen(request):
-    board = chess.Board()
-    engine = settings.STOCKFISH_ENGINE
+    try:
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        position = body['position']
+        board = chess.Board(position)
+        engine = settings.STOCKFISH_ENGINE
+    except:
+        return JsonResponse({
+            'error': 'Invalid position!'
+        })
     info = engine.analyse(board, chess.engine.Limit(time=settings.ANALYSIS_TIME))
-    score = info['score'].white().score()/100
+    print(info['score'].white().wdl().expectation())
 
     return JsonResponse({
-        'score': score
+        'score': info['score'].white().wdl().expectation()
     })
